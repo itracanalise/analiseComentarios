4fa31d70dc941c408c08a4ed12ba855d5002839c
itracanalise <92230666+itracanalise@users.noreply.github.com>
2021-11-08 14:19:42 -0300

    Separa os comentarios por nota

--- AppleExtractor.py
+++ AppleExtractor.py
@@ -1,24 +1,45 @@
 import pandas
 from app_store_scraper import AppStore
 
-app = AppStore(country="br", app_name="gov.br")
-app.review(how_many=2000)
+def get_Apple_Comments(score:int = None):
 
-dates = []
-isEdited = []
-rating = []
-review = []
-title = []
-username = []
+	if score:
+		print("Getting score: ",score)
+	else:
+		print("Getting general data")
+	app = AppStore(country="br", app_name="gov.br")
+	app.review(how_many=2000)
 
-	#fill lists with data acquired
+	dates = []
+	rating = []
+	review = []
+	title = []
 
-for line in app.reviews:
-	dates.append(line['date'])
-	rating.append(line['rating']) 
-	title.append(line['title'])  
-	review.append(line['review']) 
+		#fill lists with data acquired
 
+	if score:
+		for line in app.reviews:
+			if int(line['rating']) == score:
+				dates.append(line['date'])
+				rating.append(line['rating']) 
+				title.append(line['title'])  
+				review.append(line['review'])
 
-reviews_df = pandas.DataFrame(data={ "date": dates,"content":review,"title":title, "score":rating} )
-reviews_df.to_csv("General_Apple.csv")
+		reviews_df = pandas.DataFrame(data={ "date": dates,"content":review,"title":title, "score":rating} )
+		name  = "Apple_Score" + str(score) + ".csv"
+		reviews_df.to_csv(name)
+	else:
+		for line in app.reviews:
+			dates.append(line['date'])
+			rating.append(line['rating']) 
+			title.append(line['title'])  
+			review.append(line['review']) 
+
+		reviews_df = pandas.DataFrame(data={ "date": dates,"content":review,"title":title, "score":rating} )
+		reviews_df.to_csv("General_Apple.csv")
+
+i = 1
+while i < 6:
+	get_Apple_Comments(i)
+	i += 1
+get_Apple_Comments()
